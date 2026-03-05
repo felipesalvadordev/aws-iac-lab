ccb22454b9369a6129cf60eea82a65a452ba9c1e
Felipe Salvador <f.salvador91@gmail.com>
2026-03-05 09:09:21 -0300

    add fargate spot in auto scale

--- terraform/fargate-ecs/ecs.tf
+++ terraform/fargate-ecs/ecs.tf
@@ -1,7 +1,17 @@
-# ecs.tf
-
 resource "aws_ecs_cluster" "main" {
     name = "cb-cluster"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
 }
 
 resource "aws_ecs_task_definition" "app" {
@@ -33,7 +43,6 @@ resource "aws_ecs_service" "ecs_service" {
     cluster         = aws_ecs_cluster.main.id
     task_definition = aws_ecs_task_definition.app.arn
     desired_count   = var.app_count
-    launch_type     = "FARGATE"
 
     network_configuration {
         security_groups  = [aws_security_group.ecs_tasks.id]
@@ -48,8 +57,14 @@ resource "aws_ecs_service" "ecs_service" {
     }
 
     capacity_provider_strategy {
-      capacity_provider = "FARGATE_SPOT"
-      weight            = 100
+        capacity_provider = "FARGATE"
+        weight            = 0
+        base              = 1 # Ensure at least 1 task runs on FARGATE
+    }
+
+    capacity_provider_strategy {
+        capacity_provider = "FARGATE_SPOT"
+        weight            = 1 # Use FARGATE_SPOT for additional tasks when scaling up
     }
 
     depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs-task-execution-role-policy-attachment]
