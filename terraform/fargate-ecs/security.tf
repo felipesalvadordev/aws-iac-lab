ae6a4cc19eafdd45b915a9da42f1fa8dbb08add8
Felipe Salvador <f.salvador91@gmail.com>
2026-06-04 15:35:08 -0300

    Refine security group settings for ALB and ECS tasks
    Updated security group descriptions and ingress rules to clarify access restrictions.

--- terraform/fargate-ecs/security.tf
+++ terraform/fargate-ecs/security.tf
@@ -3,14 +3,14 @@
 # ALB security Group: Edit to restrict access to the application
 resource "aws_security_group" "lb" {
     name        = "cb-load-balancer-security-group"
-    description = "controls access to the ALB"
+    description = "Controls public access to the ALB"
     vpc_id      = aws_vpc.main.id
 
     ingress {
         protocol    = "tcp"
         from_port   = var.app_port
         to_port     = var.app_port
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"] # Open to the world, cause is public
     }
 
     egress {
@@ -24,21 +24,20 @@ resource "aws_security_group" "lb" {
 # Traffic to the ECS cluster should only come from the ALB
 resource "aws_security_group" "ecs_tasks" {
     name        = "cb-ecs-tasks-security-group"
-    description = "allow inbound access from internet"
+    description = "Allow inbound access exclusively from the ALB"
     vpc_id      = aws_vpc.main.id
 
     ingress {
         protocol        = "tcp"
         from_port       = var.app_port
         to_port         = var.app_port
-        security_groups = [aws_security_group.lb.id]
-        cidr_blocks = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.lb.id] # Allows only ALB traffic
     }
 
     egress {
         protocol    = "-1"
         from_port   = 0
         to_port     = 0
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"] # It allows tasks to download images, use SSM, call APIs, etc.
     }
 }
